5433641360bdb6718bd54097d69e938dc050ba11
Thomas <thomas.sugiarto04@gmail.com>
2025-09-11 08:15:12 +0700

    config

--- config.py
+++ config.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'distribusi_produk_secret_key'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     
     # Database configuration
-    DATABASE_URL = os.environ.get('postgres://avnadmin:AVNS_pLgUM-pJRCFOfGeM295@pg-18f2c735-kreasix-2ab4.d.aivencloud.com:16503/defaultdb?sslmode=require')
-    DB_HOST = os.environ.get('DB_HOST') or 'pg-18f2c735-kreasix-2ab4.d.aivencloud.com'
-    DB_PORT = os.environ.get('DB_PORT') or '16503'
-    DB_NAME = os.environ.get('DB_NAME') or 'defaultdb'
-    DB_USER = os.environ.get('DB_USER') or 'avnadmin'
-    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'AVNS_pLgUM-pJRCFOfGeM295'
+    DATABASE_URL = os.environ.get('DATABASE_URL')
+    DB_HOST = os.environ.get('DB_HOST') 
+    DB_PORT = os.environ.get('DB_PORT') 
+    DB_NAME = os.environ.get('DB_NAME') 
+    DB_USER = os.environ.get('DB_USER') 
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
 
 class DevelopmentConfig(Config):
     DEBUG = True
